48d4355acec2d53609134edecc167c21aea560c7
Alex <vo>
2022-04-12 09:09:43 +0300

    3. Класс хранилища пользователей UserStorage [#1104]

--- src/main/java/ru/job4j/userstrage/UserStorage.java
+++ src/main/java/ru/job4j/userstrage/UserStorage.java
@@ -16,26 +16,26 @@ public class UserStorage {
         this.users = new HashMap<>();
     }
 
-    public synchronized void add(User user) {
-        users.putIfAbsent(user.getId(), user);
+    public synchronized boolean add(User user) {
+        User result = users.putIfAbsent(user.getId(), user);
+        return result != null;
     }
 
     public synchronized boolean delete(User user) {
        return users.remove(user.getId(), user);
     }
 
-    public synchronized void update(User user) {
-        users.replace(user.getId(), user);
+    public synchronized boolean update(User user) {
+        User result = users.replace(user.getId(), user);
+        return result != null;
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         User from = users.get(fromId);
         User dest = users.get(toId);
         if (from != null && dest != null && from.getAmount() >= amount) {
-                dest.setAmount((from.getAmount() - amount) + dest.getAmount());
+                dest.setAmount(dest.getAmount() + amount);
                 from.setAmount(from.getAmount() - amount);
-                update(from);
-                update(dest);
                 return true;
         }
         return false;
